f829f13aca75ebf84eea8a4d05d95db5c81e5940
André Pinto <apinto@mi-gso.com>
2022-10-17 18:54:30 +0100

    last

--- PortfolioGitHub/react-portfolio/src/components/Sidebar/index.js
+++ PortfolioGitHub/react-portfolio/src/components/Sidebar/index.js
@@ -19,63 +19,114 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 
-const Sidebar = () =>
-{
-    const[showNav, setShowNav] = useState(false);
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
 
-return (
-    <div className= 'nav-bar'>
-        <Link className='logo' to='/' >
-            <img src={LogoS} alt="logo" />
-            <img className="sub-logo" src={LogoSubtitle} alt="s lobodan" />
-        </Link>
-        <nav> 
-            <NavLink 
-                exat="true" 
-                activeclassname="active" 
-                to= "/" >
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink 
-                exat="true" 
-                activeclassname="active" 
-                className="about-link" 
-                to= "/about" >
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink 
-                exat="true" 
-                activeclassname="active" 
-                className="contact-link" 
-                to= "/contact" >
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/andr%C3%A9-baltazar-pinto-13a056141/">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://github.com/andrepinto77">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://www.skype.com/en/">
-                    <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-        <FontAwesomeIcon
-            onClick={() => setShowNav(true)}
-            icon={faBars}
-            color="#ffd700"
-            size="3x"
-            className='hamburguer-icon' />
-        </div>
-    )
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon' />
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+  )
 }
 
 export default Sidebar
